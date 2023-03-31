50a47246a4ca39e2136a97470fa6b68b5edeeaa0
mwaney <emwaniki91@gmail.com>
2023-03-31 22:12:13 +0300

    Initial Commit

---
+++ todo-list/src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <span className="heading">TaskTrove</span>
+    </div>
+  );
+};
+
+export default App;
